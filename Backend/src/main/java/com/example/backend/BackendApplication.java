64cff167e93f3e8fc8bf4e6905b8c1cd2534f955
sajeewa garusinghe <sajeeva616@gmail.com>
2023-06-23 10:37:50 +0530

    new employee created

--- Backend/src/main/java/com/example/backend/BackendApplication.java
+++ Backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,43 @@
 package com.example.backend;
 
+import com.example.backend.model.Employee;
+import com.example.backend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+
+
+    }
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Employee employee = new Employee();
+        employee.setFirstName("Sajeewa");
+        employee.setLastName("Garusinghe");
+        employee.setEmailId("sajeeva616@gmail.com");
+        employeeRepository.save(employee);
+
+        Employee employee1 = new Employee();
+        employee1.setFirstName("Sajeewa");
+        employee1.setLastName("Garusinghe");
+        employee1.setEmailId("sajeeva616@gmail.com");
+        employeeRepository.save(employee1);
+
+        Employee employee2 = new Employee();
+        employee2.setFirstName("Sajeewa");
+        employee2.setLastName("Garusinghe");
+        employee2.setEmailId("sajeeva616@gmail.com");
+        employeeRepository.save(employee2);
     }
 
 }
